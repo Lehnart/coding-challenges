fe7886478bbc17e0b292bb4f106840bcecd359d1
Thomas <thomasserre63@gmail.com>
2018-05-09 22:49:12 +0200

    euler

---
+++ euler/26.py
@@ -0,0 +1,16 @@
+sequence_length = 0
+for i in range(2,1000):
+    found_remainders = [0]*i
+    value = 1
+    position = 0
+    while found_remainders[value] == 0 and value != 0:
+        found_remainders[value] = position
+        value *= 10
+        value %= i
+        position += 1
+
+    if position - found_remainders[value] > sequence_length:
+        sequence_length = position - found_remainders[value]
+        solution = i
+
+print("Solution : " + str(solution) )
