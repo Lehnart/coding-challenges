1c5cadced40637c9846984bffc40a3ac05fc5bcc
Thomas <thomasserre63@gmail.com>
2018-09-06 21:51:39 +0200

    76

---
+++ euler/75.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+a,b = (0,1)
+count = 0
+while True :
+    print(a)
+    a+= 1
+    b=a
+    while True :
+        b += 1
+        if a**2 + b**2 == int(a**2 + b**2) :
+            count += 1
+        if sqrt( a**2 + b**2 + (a+b)**2 )> 1500000 :
+            break
--- euler/9.py
+++ euler/9.py
@@ -1,8 +1,24 @@
-from math import sqrt
+import math
+limit = 1500000
+triangles = {}
+result = 0
+mlimit = int(math.sqrt(limit / 2))
 
-for a in range(1, 1000):
-    for b in range(1, 1000 ):
-        c = int(sqrt(a ** 2 + b ** 2))
-        if c ** 2 == a ** 2 + b ** 2 and a + b + c == 1000:
-            print("Solution : " + str(a * b * c))
-            exit()
+for m in range(2,mlimit) :
+    for n in range(1,m) :
+        if (n + m) % 2 == 1 and math.gcd(n, m) == 1 :
+            a = m * m + n * n
+            b = m * m - n * n
+            c = 2 * m * n
+            p = a + b + c
+            while p <= limit :
+                if p not in triangles :
+                    triangles[p] = 0
+                triangles[p]+=1
+                p += a + b + c
+result = 0
+for p in triangles.keys() :
+    if triangles[p] ==  1 :
+        result +=1
+
+print("Solution "+ str(result))
