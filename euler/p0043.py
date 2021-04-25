49bfa35c89d2665607040e08a59c55481817a413
Setoh <thomasserre63@gmail.com>
2021-04-25 21:51:32 +0200

    correct test 43

--- euler/p0043.py
+++ euler/p0043.py
@@ -13,7 +13,7 @@ def algo():
             digit1 = permutation_str[i]
             digit2 = permutation_str[i + 1]
             digit3 = permutation_str[i + 2]
-            if int(digit1 + digit2 + digit3) % divisors[i - 1] != 0:
+            if int(digit1 + digit2 + digit3) % divisors[i] != 0:
                 has_property = False
                 break
 
