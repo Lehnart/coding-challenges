577bf1434d96a5ba135c10052a65506b2e084735
Lehnart <thomasserre63@gmail.com>
2022-05-26 10:55:33 +0200

    solve p0700

--- problems/euler/p0700/main.py
+++ problems/euler/p0700/main.py
@@ -1,24 +1,14 @@
-def p0700(n_coins: int):
-    """
-    @param n_coins : how much euler coin to sum. If 0, sum all
-    """
-
-    found_coins = 0
-
-    n = 1
-
-    sum_coin = 0
-    c =   1504170715041707
+def p0700():
+    current_eulercoin = 1504170715041707
     mod = 4503599627370517
-    min_coin = mod
-
-    while found_coins < 1000:
-        curr = (c * n) % mod
-        if curr < min_coin:
-            min_coin = curr
-            sum_coin += curr
-            found_coins += 1
-            print("n ", found_coins, " min = ", min_coin, ", sum =", sum_coin)
-        n += 1
-
-    return sum_coin
+    lo = current_eulercoin
+    hi = current_eulercoin
+    som = current_eulercoin
+    while lo > 0:
+        ne = (lo + hi) % mod
+        if ne < lo:
+            lo = ne
+            som += lo
+        else:
+            hi = ne
+    return som
--- problems/euler/p0700/test.py
+++ problems/euler/p0700/test.py
@@ -6,4 +6,4 @@ from problems.euler.p0700.main import p0700
 class TestP0700(unittest.TestCase):
 
     def test_p0700(self):
-        self.assertEqual(p0700(2), 1513083232796311)
+        self.assertEqual(p0700(), 1517926517777556)
